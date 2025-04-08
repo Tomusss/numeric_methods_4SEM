f3ab79cb043ba6333d9ee131eec552a981375e72
Tomasz Warzecha <282261@student.pwr.edu.pl>
2025-04-08 20:21:04 +0200

    lista4

--- lista4/lista4.py
+++ lista4/lista4.py
@@ -1,6 +1,7 @@
 # zad1
 import numpy as np
-
+from scipy.sparse.linalg import gmres
+from numpy.linalg import norm
 def zadanie1():
     A = [   [1,1,2,1], 
             [0,1,4,3],
@@ -17,7 +18,24 @@ def zadanie1():
 
     print(det_a,det_b)
 
-zadanie1()
+#zadanie1()
 
 def zadanie2():
-    a = 0
+        A = np.array([
+        [1, 1/2, 1/3, 1/4, 1/5],
+        [1/2, 1/3, 1/4, 1/5, 1/6],
+        [1/3, 1/4, 1/5, 1/6, 1/7],
+        [1/4, 1/5, 1/6, 1/7, 1/8],
+        [1/5, 1/6, 1/7, 1/8, 1/9]
+        ])
+
+        b = np.array([137/60, 29/20, 153/140, 743/840, 1879/2520])
+
+        x = np.linalg.solve(A, b)
+        r = np.max(np.abs(A @ x - b))
+        
+        print("Rozwiązanie x:", x)
+        print("roznica: ", r)
+
+
+zadanie2()
